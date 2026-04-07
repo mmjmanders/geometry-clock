15096e0eeb5bdb1ef0f0b383a8e90af0a5062d6e
Mark Manders <mmjmanders@gmail.com>
2026-04-07 11:45:16 +0200

    Refactor E2E tests for efficient SVG validation and optimize workflow by removing `--only-changed` option

--- e2e/clock.spec.ts
+++ e2e/clock.spec.ts
@@ -7,41 +7,45 @@ test('should display progress indicator', async ({ page }) => {
 
 /* Test for elements visible in SVG */
 ;[
-  ...Array(24)
-    .fill(0)
-    .map((_, i) => ({
-      timestamp: new Date(`2026-04-03T${String(i).padStart(2, '0')}:00:00+02:00`),
-      hourLeftCircles: (i % 12 || 12) >= 10 ? 1 : 0,
-      hourRightCircles: (i % 12 || 12) % 10 === 0 ? 0 : (i % 12 || 12) % 10,
-      minuteLeftCircles: 0,
-      minuteRightCircles: 0,
-      secondLeftCircles: 0,
-      secondRightCircles: 0,
-    })),
-  ...Array(60)
-    .fill(0)
-    .map((_, i) => ({
-      timestamp: new Date(`2026-04-03T00:${String(i).padStart(2, '0')}:00+02:00`),
-      hourLeftCircles: 1,
-      hourRightCircles: 2,
-      minuteLeftCircles:
-        Math.floor(i / 10) === 0 ? 0 : Math.floor(i / 10) === 1 ? 1 : Math.floor(i / 10),
-      minuteRightCircles: i % 10 === 0 ? 0 : i % 10 === 1 ? 1 : i % 10,
-      secondLeftCircles: 0,
-      secondRightCircles: 0,
-    })),
-  ...Array(60)
-    .fill(0)
-    .map((_, i) => ({
-      timestamp: new Date(`2026-04-03T00:00:${String(i).padStart(2, '0')}+02:00`),
-      hourLeftCircles: 1,
-      hourRightCircles: 2,
-      minuteLeftCircles: 0,
-      minuteRightCircles: 0,
-      secondLeftCircles:
-        Math.floor(i / 10) === 0 ? 0 : Math.floor(i / 10) === 1 ? 1 : Math.floor(i / 10),
-      secondRightCircles: i % 10 === 0 ? 0 : i % 10 === 1 ? 1 : i % 10,
-    })),
+  ...new Map(
+    [
+      ...Array(24)
+        .fill(0)
+        .map((_, i) => ({
+          timestamp: new Date(`2026-04-03T${String(i).padStart(2, '0')}:00:00+02:00`),
+          hourLeftCircles: (i % 12 || 12) >= 10 ? 1 : 0,
+          hourRightCircles: (i % 12 || 12) % 10 === 0 ? 0 : (i % 12 || 12) % 10,
+          minuteLeftCircles: 0,
+          minuteRightCircles: 0,
+          secondLeftCircles: 0,
+          secondRightCircles: 0,
+        })),
+      ...Array(60)
+        .fill(0)
+        .map((_, i) => ({
+          timestamp: new Date(`2026-04-03T00:${String(i).padStart(2, '0')}:00+02:00`),
+          hourLeftCircles: 1,
+          hourRightCircles: 2,
+          minuteLeftCircles:
+            Math.floor(i / 10) === 0 ? 0 : Math.floor(i / 10) === 1 ? 1 : Math.floor(i / 10),
+          minuteRightCircles: i % 10 === 0 ? 0 : i % 10 === 1 ? 1 : i % 10,
+          secondLeftCircles: 0,
+          secondRightCircles: 0,
+        })),
+      ...Array(60)
+        .fill(0)
+        .map((_, i) => ({
+          timestamp: new Date(`2026-04-03T00:00:${String(i).padStart(2, '0')}+02:00`),
+          hourLeftCircles: 1,
+          hourRightCircles: 2,
+          minuteLeftCircles: 0,
+          minuteRightCircles: 0,
+          secondLeftCircles:
+            Math.floor(i / 10) === 0 ? 0 : Math.floor(i / 10) === 1 ? 1 : Math.floor(i / 10),
+          secondRightCircles: i % 10 === 0 ? 0 : i % 10 === 1 ? 1 : i % 10,
+        })),
+    ].map((item) => [item.timestamp.getTime(), item]),
+  ).values(),
 ].forEach(
   (
     {
@@ -55,9 +59,7 @@ test('should display progress indicator', async ({ page }) => {
     },
     i,
   ) => {
-    test(`testing visibility of SVG elements with ${timestamp.toISOString()} with index ${i}`, async ({
-      page,
-    }) => {
+    test(`testing visibility of SVG elements with ${timestamp.toISOString()}`, async ({ page }) => {
       await page.clock.setFixedTime(timestamp)
       await page.goto('/')
       await expect(page.locator('#clock-hours-left circle')).toHaveCount(hourLeftCircles)
