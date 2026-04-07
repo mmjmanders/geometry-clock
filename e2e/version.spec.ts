fb3c84c8981e3199c0ed9e0ae5b5c964a6033890
Mark Manders <mmjmanders@gmail.com>
2026-04-07 13:08:40 +0200

    Feature/move version (#9)
    * Move version display to footer and update layout, styles for consistency
    
    * Update E2E test to validate version display in footer instead of header
    
    * Add auto-merge workflow for Dependabot pull requests in GitHub Actions

--- e2e/version.spec.ts
+++ e2e/version.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test'
 
 test('should display version', async ({ page }) => {
   await page.goto('/')
-  await expect(page.locator('.version')).toHaveText(/[A-Fa-f0-9]{7}/)
+  await expect(page.locator('footer')).toHaveText(/([A-Fa-f0-9]{7})|(dev)/)
 })
